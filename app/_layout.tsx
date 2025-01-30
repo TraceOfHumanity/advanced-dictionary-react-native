0525beb29101fd77b16ac50202dbeadc469a1657
Alexander_Ovchar <azalexxx02@gmail.com>
2025-01-30 20:06:45 +0200

    Add Tailwind CSS and NativeWind for styling
    - Install Tailwind CSS and NativeWind dependencies
    - Create global.css with Tailwind directives
    - Add Tailwind configuration file
    - Update root layout to use Tailwind classes
    - Remove themed components and color constants

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
+import { Stack } from 'expo-router';
 import { Text, View } from 'react-native';
+import '../global.css';
 
 export default function RootLayout() {
   return (
-    <View>
-      <Text>Hello</Text>
+    // <Stack>
+    //   <Stack.Screen name="dictionary" options={{ headerShown: false }} />
+    // </Stack>
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-green-500 font-bold text-2xl">Hello</Text>
     </View>
   );
 }
